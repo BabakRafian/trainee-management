bfc40be7bd35922cee7f72b64ef051cb5f3e842e
Connor Mason <1552200@NOAM.TCS.com>
2018-08-24 09:44:54 -0400

    Uncommented Babak's db code. Add/View/Delete task page

--- src/server/playGround.ts
+++ src/server/playGround.ts
@@ -1,33 +1,33 @@
-// import { DBUtility } from './dbUtility';
-// import{Batch} from './models/batch';
-// import { Trainee } from './models/trainee';
-// import {City} from "./models/city";
-// //var DBUtility = require('./dbUtility');
+import { DBUtility } from './dbUtility';
+import{Batch} from './models/batch';
+import { Trainee } from './models/trainee';
+import {City} from "./models/city";
+//var DBUtility = require('./dbUtility');
 
-// let db = new DBUtility();
-// let city_pref:City={
-//     state: "New York",
-//     city:"New Jersey"
-// }
+let db = new DBUtility();
+let city_pref:City={
+    state: "New York",
+    city:"New Jersey"
+}
 
-// let tr1: Trainee = {
-//     trainee_id: 234,
-//     email: "test@na.com",
-//     first_name: "Heli",
-//     last_name: "meli",
-//     batch_id: "1",
-//     password: "1234",
-//     city_preferences: [city_pref],
-//     domain_preferences: ["Full-Stack"]
-// }
-// // db.getAllRecordsFrom('batches',(body: any[])=>{
-// //     //console.log(body[0]);
-// // });
-
-// // db.getAllRecordsFrom('trainees',(body: any[])=>{
-// //     console.log(body[0]);
-// // });
-
-// db.addNewDocument(tr1,(body:any[])=>{
-//     console.log(body);
+let tr1: Trainee = {
+    trainee_id: 234,
+    email: "test@na.com",
+    first_name: "Heli",
+    last_name: "meli",
+    batch_id: "1",
+    password: "1234",
+    city_preferences: [city_pref],
+    domain_preferences: ["Full-Stack"]
+}
+// db.getAllRecordsFrom('batches',(body: any[])=>{
+//     //console.log(body[0]);
 // });
+
+// db.getAllRecordsFrom('trainees',(body: any[])=>{
+//     console.log(body[0]);
+// });
+
+db.addNewDocument(tr1,(body:any[])=>{
+    console.log(body);
+});
